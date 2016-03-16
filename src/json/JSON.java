0ce4e89bce7f3b5804f27f3414e402c820e8d34f
CH <CH@CH-PC>
2016-03-15 23:58:21 -0300

    dando paw nao sei pq :(

--- src/json/JSON.java
+++ src/json/JSON.java
@@ -36,26 +36,45 @@ public class JSON {
     public static void main(String[] args) {
 
         String url = "http://i3geo.saude.gov.br/i3geo/ogc.php?service=WFS&version=1.0.0&request=GetFeature&typeName=upa_funcionamento_cnes&outputFormat=JSON";
-        JSONArray matches;
+        String json = "{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"properties\":[{\"gid\":\"7206445\"},{\"co_cep\":\"14150-000\"},{\"uf\":\"SP\"},{\"cidade\":\"Serrana\"},{\"no_fantasia\":\"UPA DR GERALDO C P REIS\"},{\"no_bairro\":\"JD ROMULO MONTANARI\"},{\"nu_endereco\":\"235\"},{\"no_logradouro\":\"GABRIEL JOSE DO VALE\"},{\"nu_telefone\":\"(16) 3987-1513\"},{\"ano_upa_func\":\"2016\"},{\"mes_upa_func\":\"1\"},{\"fonte_recurso\":\"MS\"},{\"porte\":\"1\"}],\"geometry\":{\"type\":\"Point\",\"coordinates\":[-47.602643966675,-21.208098872715]}}]}";
+ 
+//        JSONObject objPrincipal;
+//        JSONArray arrayFeatures;
+//        JSONArray arrayProperties;
         try {
-            matches = new JSONArray(readUrl(url));
-            for (int i = 0; i < matches.length(); i++) {
-                JSONObject match = matches.getJSONObject(i);                
-                JSONObject home = match.getJSONObject("home_team");
-                JSONObject away = match.getJSONObject("away_team");
+            JSONObject objPrincipal = new JSONObject(readUrl(url));
+            JSONArray arrayFeatures =  objPrincipal.getJSONArray("features");
+            for (int i = 0; i < arrayFeatures.length(); i++) 
+            {
+                JSONObject obj = arrayFeatures.getJSONObject(i);
+                System.out.println("id: " + (i+1) + " type: " + obj.getString("type"));
+                JSONArray arrayProperties = obj.getJSONArray("properties");
                 
-                String location = match.getString("location");
-                String homeTeam = home.getString("country");
-                int homeTeamGoals = home.getInt("goals");                
-                String awayTeam = away.getString("country");
-                int awayTeamGoals = away.getInt("goals");
-
-                System.out.printf("Jogo %d: %s %d x %d %s [%s] \n", 
-                    i+1, homeTeam, homeTeamGoals, awayTeamGoals, awayTeam, location);
+                for(int a = 0; a < arrayProperties.length(); a++)
+                {
+                   JSONObject objPro = arrayProperties.getJSONObject(a);
+                   System.out.println("gid: " + objPro.getString("gid"));
+                    
+//                    JSONObject gid = objPro.getJSONObject("gid");
+//                    System.out.println("cep: " + gid.getString("co_cep"));
+//                    System.out.println("uf: " + gid.getString("uf"));
+//                    System.out.println("cidade: " + gid.getString("cidade"));
+//                    System.out.println("nome: " + gid.getString("no_fantasia"));
+//                    System.out.println("bairro: " + gid.getString("no_bairro"));
+//                    System.out.println("endereço: " + gid.getString("nu_endereco"));
+//                    System.out.println("rua: " + gid.getString("no_logradouro"));
+//                    System.out.println("telefone: " + gid.getString("nu_telefone"));
+//                    System.out.println("ano: " + gid.getString("ano_upa_func"));
+//                    System.out.println("mês: " + gid.getString("mes_upa_func"));
+//                    System.out.println("recurso: " + gid.getString("fonte_recurso"));
+//                    System.out.println("porte: " + gid.getString("porte"));
+                }
             }
-        } catch (JSONException e) {
+        } catch (JSONException e) 
+        {
             System.out.println("Erro JSON: " + e.getMessage());
-        } catch (Exception e) {
+        } catch (Exception e) 
+        {
             System.out.println("Erro geral: " + e.getMessage());
         }
     }
